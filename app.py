2614c085e7efbcff2a7eb7cfc7ef1d536c723eb4
maaji77 <maahijoshua81@gmail.com>
2024-08-28 14:05:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -88,8 +88,6 @@ st.write(f"You selected: {option}")
 if st.sidebar.checkbox("Show additional information"):
     st.sidebar.write("This is a chatbot created to help you in getting information on surgeries done in the Sir Lester Bird Medical Center. It gives you a detailed description on the surgery types, how to prepare for a certain surgery, and provides accurate prices of different aspects of the surgery. It also gives you the most common surgery types for each age group. This chatbot is the most efficient way of data access from SLBMC as it is eay to use, very quick and informative, saving you a visit to the doctor. I hope this helps give a general idea of MedAssist. ")
 
-import streamlit as st
-
 big_logo = "logo__2_-removebg-preview (1).png"
 logo = "single_logo-removebg-preview.png"
 
